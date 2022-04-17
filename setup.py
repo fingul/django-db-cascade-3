c75be9a784764d9f44fd7c96621fbf5c0e12806c
fingul <fingul@gmail.com>
2022-04-18 00:39:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-db-cascade-2',
+    name='django-db-cascade-3',
     version='0.3.1',
-    description='Fork of django-db-cascade for Django 3.0.  Optionally use postgres db ON CASCADE DELETE on django foreign keys',
-    url='http://github.com/jkapelner/django-db-cascade-2',
-    author='Nick Stefan, Jordan Kapelner',
-    author_email='nickstefan12@gmail.com',
+    description='Fork of django-db-cascade for Django 3.0+.  Optionally use postgres db ON CASCADE DELETE/SET NULL on django foreign keys',
+    url='http://github.com/fingul/django-db-cascade-3',
+    author='Fingul',
+    author_email='fingul@gmail.com',
     license='MIT',
     packages=find_packages(),
     install_requires=[
